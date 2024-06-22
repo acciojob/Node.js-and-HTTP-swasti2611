93b23954af896bfb2f4cc2ed6b002929f9ce5628
Swati jagtap <swatijagtap5060@gmail.com>
2024-06-22 16:06:26 +0530

    submit-1

--- main.js
+++ main.js
@@ -2,6 +2,14 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   // TODO: Implement this function
+  if(req.url==="/"){
+    if(req.method==='GET'){
+      res.end(JSON.stringify({message:"this is get request"}))
+    }
+    else{
+      res.end(JSON.stringify({message:"this is post request"}))
+    }
+  }
 });
 
 server.listen(3000, () => {
